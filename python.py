3d34411d1f3b2cb59c9037776a1b033dcf074f86
hellopanhakoko <shensi261207@gmail.com>
2025-11-15 19:48:20 +0700

    Update python.py

--- python.py
+++ python.py
@@ -1,34 +1,19 @@
-from flask import Flask, request, jsonify
-import requests
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from werkzeug.serving import run_simple
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-NEW_API_URL = "https://mengtopup.shop/api/check_payment?md5={md5}"
+# Root route
+@app.get("/")
+def root():
+    return {"message": "Welcome to the root route!"}
 
+# Route to display info for items
+@app.get("/items/")
+def items():
+    return {"message": "Welcome to the items route!"}
 
-@app.get("/check!payment")
-def check_payment():
-    target_url = f"{NEW_API_URL}"
+# Route to display info for updating items
+@app.get("/items/update/")
+def update_item():
+    return {"message": "Welcome to the update item route!"}
 
-    try:
-        response = requests.get(target_url)
-        return jsonify(response.json())
-    except Exception as e:
-        return jsonify({"success": False, "error": str(e)})
-
-
-@app.get("/dev")
-def dev():
-    return jsonify({"dev": "made@by@panha"})
-
-
-@app.get("/infor")
-def infor():
-    return jsonify({"infor": "trueid26"})
-
-
-# Vercel requires `app` to be named `app`
-# This line exposes Flask app to Vercel serverless
-app = app
